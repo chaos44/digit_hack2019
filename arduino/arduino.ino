1ff63f0069bb36bc0df5d3702b79ed28dfcdc97d
dracen <a.hayashi0113@gmail.com>
2019-05-31 02:28:09 +0900

    TeraTermからモータ回せること確認

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1120,7 +1120,84 @@ void command_characteristic_handler() {
   }
 }
 
+
+
+#define CHARBUFFERSIZE 100
+char Buffer[100];
+
+boolean readSerial() {
+  static byte index = 0;                // Buffer array index counter
+  
+  while (Serial.available() > 0) {     // Does serial1 have characters available?
+    char rc = Serial.read();           // Read a character
+    
+    if (rc == '\n') {                   // Is it newline?
+      Buffer[index] = 0;           // Yes, so terminate char array with 0
+      index = 0;                        // Reset array index
+      return true;                      // Return true (we have a full line)
+    }
+    if (true) {  
+    // if (rc >= '0') {                    // Is value between 32 and 127?
+      Buffer[index++] = rc;        // Yes, so store it and increment index
+      if (index > CHARBUFFERSIZE) {     // Have we filled the buffer?
+        index--;                        // Yes, so decrement index
+      }
+    }
+  }
+  return false;                         // Return false (we don't have a full line)
+}
+
+int strToInt(char *str)
+{
+    char c;
+    int result = 0;
+    int sign = 1;   // default to no minus. 
+    // Check for minus
+    if (str[0] == '-') 
+    {
+      str++;   // Skip over the minus.
+      sign = -1;  // It's a negative number. 
+    }
+ 
+    while((c = *str))   // Stop the loop if we are at the end of the string.
+    {
+       // See if it's valid digit
+       if (c >= '0' && c <= '9') 
+       {
+          result *= 10;
+          result += c - '0';
+       }
+       else // Not a valid digit
+       {
+           break; // Stop looking for more digits. 
+       }
+       str++;  // Go to next digit. 
+    }
+    return result * sign;
+}
+
 void loop() {
+  int motor;
+  boolean ret = readSerial();
+  if(ret==true){
+    if(Buffer[0]=='m'){
+      if(Buffer[1]=='0'){
+        motor = 0;
+        Serial.print("m0:");
+      }else if(Buffer[1]=='1'){
+         motor = 1;
+        Serial.print("m1:");
+      }
+      uint8_t speed = strToInt(&Buffer[3]);
+      Serial.println(speed);
+      
+      cmd_motor(motor, speed);
+    }
+    Serial.println(Buffer);
+
+    //static void cmd_motor(uint8_t motor_no, int8_t speed) {
+  }
+
   // Change service UUID
   if (g_flag_update_advertiseuuid) {
     update_advertiseuuid();
@@ -1310,8 +1387,7 @@ void notify_board_state(float acc_x, float acc_y, float acc_z, float temperature
 void device_init() {
   Wire.begin();
 }
-
-
+ 
 
 static const int motor_addr = 0x26;
 static void cmd_motor(uint8_t motor_no, int8_t speed) {
